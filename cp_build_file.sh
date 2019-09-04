16a01504d07a06f210667ad173547e9d5be1b4e3
n350071 <n350071@gmail.com>
2019-09-05 03:52:10 +0900

    :bug: step2 was wrong.

--- cp_build_file.sh
+++ cp_build_file.sh
@@ -1,6 +1,5 @@
-cp -r ./rails-starter-kit-with-docker .
+cp -r ./rails-starter-kit-with-docker/* .
 rm screenshot.png
 rm -rf .git
 rm -rf rails-starter-kit-with-docker/
-
-rm cp_build_file.sh
+rm cp_build
